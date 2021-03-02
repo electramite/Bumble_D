ee5c7ed7494413b7acfa0c7a05cadb8aede3ea09
shashwat <electramite@gmail.com>
2021-03-02 15:51:34 +0530

    Create rpi.py

---
+++ interfacing_of_fsi6_rcvr_with_arduino/fsi6_servo_control_via_serial_communication_between_arduino_and_rpi/rpi.py
@@ -0,0 +1,27 @@
+import socket
+import serial
+import time
+import re
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+ser.flush()
+
+
+s = socket.socket()
+host = "192.168.43.70"
+port = 4592
+s.bind((host, port))
+time.sleep(1)
+
+print(host)
+s.listen(5)
+c, addr = s.accept()
+while True:
+
+	
+	d1 = c.recv(1024).decode('utf-8')	
+	d1 = re.findall(r'\d+', d1)
+	
+	D1 = list(map(int, d1))
+	name = str(D1[0])
+	ser.write(f"{name}\n".encode('utf-8'))
