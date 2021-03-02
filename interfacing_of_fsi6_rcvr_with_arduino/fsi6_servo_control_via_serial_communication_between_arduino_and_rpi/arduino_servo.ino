75a0105fe6968fa9ca5b0493c76a88fe1251dacf
shashwat <electramite@gmail.com>
2021-03-02 15:53:12 +0530

    Create arduino_servo.ino

---
+++ interfacing_of_fsi6_rcvr_with_arduino/fsi6_servo_control_via_serial_communication_between_arduino_and_rpi/arduino_servo.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+Servo bldc;
+
+
+void setup() {
+  Serial.begin(9600);
+  bldc.attach(9);
+}
+void loop() {
+  if (Serial.available() > 0) {
+
+    String data = Serial.readStringUntil('\n');
+    int x = data.toInt();
+    x = map(x, 975, 1990, 0, 180);
+    bldc.write(x);    
+  }
+}
