074628d12264adc1b3850e73c8779d8f840d7663
shashwat <electramite@gmail.com>
2021-03-08 05:20:57 +0530

    Update rcvr_RPi4_code.py

--- interfacing_of_fsi6_rcvr_with_arduino/fsi6_data_on_rpi_socket_v2/rcvr_RPi4_code.py
+++ interfacing_of_fsi6_rcvr_with_arduino/fsi6_data_on_rpi_socket_v2/rcvr_RPi4_code.py
@@ -24,15 +24,6 @@ while True:
     d1 = re.findall(r'\d+', d1) # storing them in a list
 
     print(d1)
-    ch1 = str(d1[0]) # extracting PWM reading for Ch1
-    ser.write(f"{ch1}\n".encode('utf-8')) # sending PWM signal for Ch1 to rcvr_arduino which is connected to RPi via serial communication
-    ch2 = str(d1[1]) # extracting PWM reading for Ch2
-    ser.write(f"{ch2}\n".encode('utf-8')) # sending PWM signal for Ch2 to rcvr_arduino 
-    ch3 = str(d1[2]) # extracting PWM reading for Ch3
-    ser.write(f"{ch3}\n".encode('utf-8'));
-    ch4 = str(d1[3]) # extracting PWM reading for Ch4
-    ser.write(f"{ch4}\n".encode('utf-8'))
-    ch5 = str(d1[4]) # extracting PWM reading for Ch5
-    ser.write(f"{ch5}\n".encode('utf-8'))
-    ch6 = str(d1[5]) # extracting PWM reading for Ch6
-    ser.write(f"{ch6}\n".encode('utf-8')) 
+    ch = f"{d1[0]} {d1[1]} {d1[2]} {d1[3]} {d1[4]} {d1[5]}\n"
+    ser.write(f"{ch}\n".encode('utf-8'))
+
